06c84ce72350789ad2c954ee2ce56f8e91741499
Jocter <ggaranha2002@gmail.com>
2018-12-09 18:48:12 -0200

    Uploading minor changes

---
+++ 1310692-1320755/src/diceRules.java
@@ -0,0 +1,38 @@
+
+public class diceRules {
+	
+	static boolean pawnChanged;
+	
+	public diceRules()
+	{
+		
+	}
+	
+	public static boolean diceMovement(game mainGame, dice gameDice)
+	{
+		pawnChanged = autoMovement(mainGame, gameDice.getValueDice());
+		
+		return pawnChanged;
+	}
+	
+	private static boolean autoMovement(game mainGame, int diceValue)
+	{
+		pawn pRef;
+		tile tRef;
+		
+		if(diceValue==5)
+		{
+			pRef = mainGame.getCurrentPlayer().getPawnOnBeginTile();
+			
+			if(pRef != null)
+			{	
+				tRef = mainGame.getCurrentPlayer().getPlayerExitTile();
+				tRef.addTilePawn(pRef);
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+}
